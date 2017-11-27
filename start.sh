af46b97446cbe36513799539b2bb1dc99e3de58a
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2017-11-27 20:42:17 +0100

    Simplified start script

--- start.sh
+++ start.sh
@@ -1,50 +1,9 @@
 #!/bin/bash
 # script to bootstrap setting up a mac with ansible 
 
-function uninstall {
-
-echo "WARNING : This will remove homebrew and all applications installed through it"
-echo -n "are you sure you want to do that? [y/n] : "
-read confirmation
-
-if [ $confirmation == "y" ]; then
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
-    exit 0
-else
-  echo "keeping everything intact"
-  exit 0
-fi
-
-}
-
-if [ $1 == "uninstall" ]; then
-    uninstall
-fi
-
-echo "==========================================="
-echo "Setting up your mac using bentsolheim/setupmac"
-echo "==========================================="
-
 sudo easy_install pip
 sudo pip install ansible
 
-installdir="/tmp/setupmac-$RANDOM"
-mkdir $installdir
-
-git clone https://github.com/bentsolheim/setupmac.git $installdir
-if [ ! -d $installdir ]; then
-    echo "failed to find setupmac."
-    echo "git cloned failed"
-    exit 1
-else
-    cd $installdir 
-    ansible-playbook playbook-osx.yml --verbose --ask-sudo-pass
-fi
-
-echo "cleaning up..."
-
-rm -Rfv /tmp/$installdir
-
-echo "and we are done! Enjoy!"
+ansible-playbook playbook-osx.yml --verbose --ask-sudo-pass
 
 exit 0
